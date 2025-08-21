3e55cb92b5c22efe053f0ad3af3efc561ba4802c
sheikhmubashi <hammadyounus.508@gmail.com>
2025-08-21 21:11:42 +0500

    Completed!

--- src/components/InfiniteSliderBasic.tsx
+++ src/components/InfiniteSliderBasic.tsx
@@ -4,8 +4,8 @@ import { InfiniteSlider } from "./ui/InfiniteSlider";
 export function InfiniteSliderBasic() {
  return (
   <div className="md:h-32 bg-[#F4F4F4] ">
-   <div className=" flex md:flex-row flex-col items-center justify-between md:ps-40">
-    <div className="md:w-[400px] md:pe-16 p-4 ">
+   <div className=" flex md:flex-row flex-col items-center justify-between md:pl-40">
+    <div className="md:w-[400px] md:pr-16 p-4 ">
      <p className="text-[#1E1E1E]">
       Helping investors reclaim funds from failed or fraudulent real estate
       projects across Dubai.
